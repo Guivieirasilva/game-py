f3735281f984ead9d71b0e351cdd433d5df052c6
gui vieira <guivieirasilva02@gmail.com>
2025-05-29 06:46:48 -0300

    Feat: Hero with special attack and enemy attacking

--- game.py
+++ game.py
@@ -16,6 +16,18 @@ class Character:
     def show_details(self):
         return f"Nome: {self.__name}\nVida: {self.__life}\nNível: {self.__level}"
 
+    def receive_attack(self, damage):
+        self.__life -= damage
+        if self.__life < 0:
+            self.__life = 0
+
+    def attack(self, target):
+        damage = self.__level * 2
+        target.receive_attack(damage)
+        print(
+            f"{self.get_name()} atacou {target.get_name()} e causou {damage} de dano!"
+        )
+
 
 class Hero(Character):
     def __init__(self, name, life, level, special):
@@ -27,6 +39,14 @@ class Hero(Character):
 
     def show_details(self):
         return f"{super().show_details()}\nHabilidade: {self.get_special()}\n"
+
+    def attack_special(self, target):
+        damage = self.get_level() * 5
+        target.receive_attack(damage)
+        print(
+            f"{self.get_name()} usou a habilidade especial: {self.get_special()}",
+            f"no {target.get_name()} e causou {damage} de dano!",
+        )
 
 
 class Enemy(Character):
@@ -45,19 +65,39 @@ class Game:
     """Classa Orquestadora do Jogo"""
 
     def __init__(self) -> None:
-        self.hero = Hero(name="Guilherme", life=100, level=5, special="Kamehamera")
+        self.hero = Hero(name="Goku", life=100, level=5, special="Kamehamera")
         self.enemy = Enemy(name="Black", life=100, level=4, type="SSR")
 
     def initBatle(self):
         """Fazer a gestão da batalha"""
         print("Iniciando a batalha")
+
         while self.hero.get_life() > 0 and self.enemy.get_life() > 0:
             print("\nDetalhe dos personagens: ")
             print(f"{self.hero.show_details()}")
             print(f"{self.enemy.show_details()}")
 
-        input("Pressione ENTER para atacar...")
-        escolha = input("1 - Ataque normal | 2 - Especial: ")
+            input("Pressione ENTER para atacar...")
+            select = int(input("1 - Ataque normal | 2 - Especial: "))
+
+            if select == 1:
+                self.hero.attack(self.enemy)
+
+            elif select == 2:
+                self.hero.attack_special(self.enemy)
+
+            else:
+                print("Escolha inválida. Escolha novamente!")
+
+            if self.enemy.get_life() > 0:
+                """Inimigo ataca o heroi"""
+                self.enemy.attack(self.hero)
+
+        if self.hero.get_life() > 0:
+            print("Parabéns você foi o vencedor!")
+
+        else:
+            print("Você foi derrotado")
 
 
 game = Game()
